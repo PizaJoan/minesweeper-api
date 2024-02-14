4e9d03bcbe0b0d217737970890f843f5e409b00f
Juan Piza Ferra <pizajoan97@gmail.com>
2024-02-14 18:11:51 +0100

    updating or creating users with google from client

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,17 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('/init')
-  async initUser(@Query('userId') userId: number) {
+  async initUser(
+    @Query('userId') userId: number,
+    @Query('token') token: string,
+  ) {
+    // Oauth flow
+    if (token) {
+      const user = await this.userService.initUserWithToken(token, userId);
+      return user.name;
+    }
+
+    // Bare user
     if (!userId) {
       const user = await this.userService.initUser();
       return user.id;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,13 +7,33 @@ export class UserService {
   constructor(private userRepository: UserRepository) {}
 
   async initUser() {
-    const randomUser = await fetch('https://api.namefake.com/');
-    const { name } = await randomUser.json();
+    return await this.userRepository.createUser(await this.getRandomName());
+  }
+
+  async initUserWithToken(token: string, userId?: number) {
+    // decode JWT Token
+    const name = `${token}`;
+
+    if (userId) {
+      const user = await this.userRepository.findById(userId);
+
+      user!.name = name;
+      await user?.save();
+
+      return user!;
+    }
 
     return await this.userRepository.createUser(name);
   }
 
   async findById(userId: number) {
     return await this.userRepository.findById(userId);
+  }
+
+  private async getRandomName() {
+    const randomUser = await fetch('https://api.namefake.com/');
+    const { name } = await randomUser.json();
+
+    return name;
   }
 }
