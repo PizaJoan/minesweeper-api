adb1ba62e0646995c0411393d9ba44975c356164
Juan Piza Ferra <pizajoan97@gmail.com>
2024-02-20 10:32:26 +0100

    StringToInteger pipe to handle array of objects

--- src/decorators/pipes/stringToInteger.ts
+++ src/decorators/pipes/stringToInteger.ts
@@ -1,9 +1,23 @@
 import { PipeTransform } from '@nestjs/common';
 
+function transformObject(value: any) {
+  if (typeof value === 'object') {
+    for (const key in value) {
+      value[key] = Number(value[key]);
+    }
+  } else {
+    value = Number(value);
+  }
+
+  return value;
+}
+
 export class StringToInteger implements PipeTransform {
   transform(value: any) {
     for (const key in value) {
-      value[key] = Number(value[key]);
+      if (Array.isArray(value[key])) {
+        value[key] = value[key].map(transformObject);
+      } else value[key] = transformObject(value[key]);
     }
 
     return value;
